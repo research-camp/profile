58eaa0b29178b70752f667818b7cfb757c964e29
Official21A <officialamirhossein21@gmail.com>
2020-10-30 11:06:20 +0330

    Domain names find

---
+++ test.py
@@ -0,0 +1,16 @@
+import mechanicalsoup
+
+url = 'https://www.theguardian.com/football/tables'
+
+browser = mechanicalsoup.Browser()
+
+front_page = browser.get(url)
+soup_front = front_page.soup
+
+links = soup_front.findAll('a', {'class':'full-table-link'})
+
+urls_to_follow = []
+
+for link in links:
+	new_url = front_page.url[:-16] + link['href']
+	urls_to_follow.append(new_url)
