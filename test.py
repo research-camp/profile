6b695695f01d7eff48fca7a26b2d465891c2ea03
Official21A <officialamirhossein21@gmail.com>
2020-11-01 07:30:40 +0330

    Refactor

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
 import mechanicalsoup
 
-url = 'https://www.theguardian.com/football/tables'
+url = 'https://www.theguardian.com/football/tables' # web site we use for data
 
 browser = mechanicalsoup.Browser()
 
 front_page = browser.get(url)
 soup_front = front_page.soup
 
-links = soup_front.findAll('a', {'class':'full-table-link'})
+links = soup_front.findAll('a', {'class':'full-table-link'}) # other pages urls
 
-urls_to_follow = []
+urls_to_follow = [] # we modify the urls in a pattern that works correctly
 
 for link in links:
-	new_url = front_page.url[:-16] + link['href']
+	new_url = front_page.url[:-16] + link['href'] # create a full domain name
 	urls_to_follow.append(new_url)
 
-for web in urls_to_follow[:6]: # the pattern changes after 6
+for web in urls_to_follow[:6]: # the pattern changes after row 6
 
 	new_browser = mechanicalsoup.Browser()
 	league = new_browser.get(web)
 
-	rows = league.soup.findAll('tr')
-	name = league.soup.find('caption')
+	rows = league.soup.findAll('tr') # get all the table lines
+	name = league.soup.find('caption') # get the league name
 
 	print(f'\n>> {name.text.strip()}')
 
-	for i in range(1,len(rows)):
+	for i in range(1,len(rows)): # an iteration over table rows
 		print(f'{i}.', end="")
 		try:
 			link = rows[i].find('a')
@@ -48,7 +48,7 @@ for web in urls_to_follow[6:9]: # this is the middle tables in different pattern
 
 	print(f'\n>> {name.text.strip()}')
 
-	for div in divisons:
+	for div in divisons: # these pages have a bit different pattern
 		print(f'{div.h4.text.strip()}')
 
 		rows = div.findAll('tr')
@@ -67,7 +67,7 @@ for web in urls_to_follow[6:9]: # this is the middle tables in different pattern
 			points = rows[i].find('b')
 			print(f" : {points.text.strip()}")
 
-for web in urls_to_follow[9:-2]: # the pattern changes after 6
+for web in urls_to_follow[9:-2]: # the normal pattern
 
 	new_browser = mechanicalsoup.Browser()
 	league = new_browser.get(web)
@@ -91,7 +91,7 @@ for web in urls_to_follow[9:-2]: # the pattern changes after 6
 		points = rows[i].find('b')
 		print(f" : {points.text.strip()}")
 
-for web in urls_to_follow[-2:]: # this is the middle tables in different pattern
+for web in urls_to_follow[-2:]: # this is the last2 tables in different pattern
 	new_browser = mechanicalsoup.Browser()
 	league = new_browser.get(web)
 
