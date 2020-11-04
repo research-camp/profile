66b8da73e7b9d148ff2c01f3ade9f0e6cf07bb4c
Official21A <officialamirhossein21@gmail.com>
2020-11-04 07:52:05 +0330

    Start html

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import os
 import datetime
 
+from prettytable import PrettyTable
+
 
 MAIN_DIR = "./docs/"
 FORMAT = ".txt"
+TABLE_FORMAT = ".html"
 DATE_PATTERN = "%B %d %Y - %I:%M %p"
 
 
@@ -15,20 +18,24 @@ def dir_check(path):
 def init_file():
 	year_dir = datetime.datetime.now().strftime("%Y") + "/"
 	month_dir = datetime.datetime.now().strftime("%m") + "/"
-	name = datetime.datetime.now().strftime("%d")
+	name = datetime.datetime.now().strftime("%d") + "/"
 	dir_check(MAIN_DIR)
 	dir_check(MAIN_DIR + year_dir)	
-	dir_check(MAIN_DIR + year_dir + month_dir)	
-	file_path = MAIN_DIR + year_dir + month_dir + name + FORMAT
+	dir_check(MAIN_DIR + year_dir + month_dir)
+	dir_check(MAIN_DIR + year_dir + month_dir + name)	
+	file_path = MAIN_DIR + year_dir + month_dir + name + "doc" + FORMAT
+	html_path = MAIN_DIR + year_dir + month_dir + name + "table" + TABLE_FORMAT
 	with open(file_path, 'w') as file:
 		file.write(f'> {datetime.datetime.now().strftime(DATE_PATTERN)}\n\n')
+	with open(html_path, 'w') as file:
+		file.write(f'<h2>{datetime.datetime.now().strftime(DATE_PATTERN)}</h2>')
 
 
 def save_to_now(string):
 	year_dir = datetime.datetime.now().strftime("%Y") + "/"
 	month_dir = datetime.datetime.now().strftime("%m") + "/"
-	name = datetime.datetime.now().strftime("%d")
-	file_path =MAIN_DIR + year_dir + month_dir + name + FORMAT
+	name = datetime.datetime.now().strftime("%d") + "/"
+	file_path =MAIN_DIR + year_dir + month_dir + name + "doc" + FORMAT
 	with open(file_path, 'a+') as file:
 		file.write(string)
 	
